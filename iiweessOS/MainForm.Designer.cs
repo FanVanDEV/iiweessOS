0b3ba21b5fd27a51717ebe1a073075307bad87e2
Fan Van <ya.fanvan@yandex.ru>
2024-11-15 00:21:14 +0300

    feat(Main form): initial design

---
+++ iiweessOS/MainForm.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace iiweessOS
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.headPanel = new System.Windows.Forms.Panel();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.collapseButton = new System.Windows.Forms.Button();
+            this.resizeButton = new System.Windows.Forms.Button();
+            this.headPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(23)))), ((int)(((byte)(41)))));
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("Consolas", 14F);
+            this.richTextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(170)))), ((int)(((byte)(100)))));
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(800, 450);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "asdasdsdasadasdsdadsasdasdasdasda";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // headPanel
+            // 
+            this.headPanel.AutoSize = true;
+            this.headPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(23)))), ((int)(((byte)(41)))));
+            this.headPanel.Controls.Add(this.closeButton);
+            this.headPanel.Controls.Add(this.collapseButton);
+            this.headPanel.Controls.Add(this.resizeButton);
+            this.headPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.headPanel.Location = new System.Drawing.Point(0, 0);
+            this.headPanel.Name = "headPanel";
+            this.headPanel.Size = new System.Drawing.Size(800, 48);
+            this.headPanel.TabIndex = 1;
+            this.headPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.closeButton.BackColor = System.Drawing.Color.Transparent;
+            this.closeButton.FlatAppearance.BorderSize = 0;
+            this.closeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.closeButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(145)))), ((int)(((byte)(165)))));
+            this.closeButton.Location = new System.Drawing.Point(756, 3);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(32, 41);
+            this.closeButton.TabIndex = 2;
+            this.closeButton.Text = "❌";
+            this.closeButton.UseVisualStyleBackColor = false;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // collapseButton
+            // 
+            this.collapseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.collapseButton.BackColor = System.Drawing.Color.Transparent;
+            this.collapseButton.FlatAppearance.BorderSize = 0;
+            this.collapseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.collapseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.collapseButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(145)))), ((int)(((byte)(165)))));
+            this.collapseButton.Location = new System.Drawing.Point(653, 0);
+            this.collapseButton.Name = "collapseButton";
+            this.collapseButton.Size = new System.Drawing.Size(32, 45);
+            this.collapseButton.TabIndex = 1;
+            this.collapseButton.Text = "–";
+            this.collapseButton.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.collapseButton.UseVisualStyleBackColor = false;
+            this.collapseButton.Click += new System.EventHandler(this.collapseButton_Click);
+            // 
+            // resizeButton
+            // 
+            this.resizeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.resizeButton.BackColor = System.Drawing.Color.Transparent;
+            this.resizeButton.FlatAppearance.BorderSize = 0;
+            this.resizeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.resizeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resizeButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(145)))), ((int)(((byte)(165)))));
+            this.resizeButton.Location = new System.Drawing.Point(705, 0);
+            this.resizeButton.Name = "resizeButton";
+            this.resizeButton.Size = new System.Drawing.Size(32, 44);
+            this.resizeButton.TabIndex = 0;
+            this.resizeButton.Text = "🗖";
+            this.resizeButton.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.resizeButton.UseVisualStyleBackColor = false;
+            this.resizeButton.Click += new System.EventHandler(this.resizeButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.headPanel);
+            this.Controls.Add(this.richTextBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "iiweessOS — Shell emulator for operating system";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.headPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel headPanel;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Button collapseButton;
+        private System.Windows.Forms.Button resizeButton;
+    }
+}
+
---
+++ iiweessOS/MainForm.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace iiweessOS
+{
+    public partial class MainForm : Form
+    {
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int msg, int wParam, int lParam);
+
+        private const int WM_NCLBUTTONDOWN = 0xA1;
+        private const int HTCAPTION = 0x2;
+
+        public MainForm()
+        {
+            StyleForm();
+            InitializeComponent();
+            InitPanel();
+
+            this.Resize += MainForm_Resize;
+            this.SizeChanged += MainForm_SizeChanged;
+        }
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            UpdateMaximizeButton();
+        }
+
+        private void MainForm_SizeChanged(object sender, EventArgs e)
+        {
+            UpdateMaximizeButton();
+        }
+        private void UpdateMaximizeButton()
+        {
+            if (this.WindowState == FormWindowState.Maximized)
+            {
+                resizeButton.Text = "🗗";
+            }
+            else if (this.WindowState == FormWindowState.Normal)
+            {
+                resizeButton.Text = "🗖";
+            }
+        }
+
+        private void InitPanel()
+        {
+            this.MouseDown += new MouseEventHandler(Form_MouseDown);
+            headPanel.MouseDown += new MouseEventHandler(Form_MouseDown);
+        }
+
+        private void Form_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(this.Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
+            }
+        }
+
+        private void StyleForm()
+        {
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.ControlBox = false;
+            this.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void collapseButton_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+        private void resizeButton_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Normal)
+            {
+                this.WindowState = FormWindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+            UpdateMaximizeButton();
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
