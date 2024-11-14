995e87b6182c0349c4eb24d990576045192decc5
Fan Van <ya.fanvan@yandex.ru>
2024-11-15 00:42:07 +0300

    feat(project): init MVC model

---
+++ iiweessOS/Controllers/ShellController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iiweessOS.Controllers
+{
+    public class ShellController
+    {
+    }
+}
