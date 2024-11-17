62c0592563d9807b05278074828a0742caff2b20
Fan Van <ya.fanvan@yandex.ru>
2024-11-17 04:42:34 +0300

    feat(commands): shell controller + date command

--- iiweessOS/Commands/DateCommand.cs
+++ iiweessOS/Commands/DateCommand.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace iiweessOS.Commands
@@ -10,12 +11,18 @@ namespace iiweessOS.Commands
     {
         public string Execute(string[] args)
         {
-            throw new NotImplementedException();
+            string format = "ddd MMM d H:mm:ss yyyy";
+            if (args.Length >= 1 && Regex.IsMatch(args[0], @"\+""(.*?)""")) // +"(any)"
+            {
+                format = args[0].Split('"')[1];
+            }
+
+            return DateTime.Now.ToString(format);
         }
 
         public string GetHelp()
         {
-            throw new NotImplementedException();
+            return "Usage: date ";
         }
     }
 }
--- iiweessOS/Controllers/ShellController.cs
+++ iiweessOS/Controllers/ShellController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using iiweessOS.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,21 @@ namespace iiweessOS.Controllers
 {
     public class ShellController
     {
+        private readonly CommandFactory _commandFactory;
+
+        public ShellController(CommandFactory commandFactory)
+        {
+            _commandFactory = commandFactory;
+        }
+
+        public string ExecuteCommand(string input)
+        {
+            var parts = input.Trim().Split(' ');
+            var commandName = parts[0];
+            var args = parts.Length > 1 ? parts.Skip(1).ToArray() : new string[0];
+
+            var command = _commandFactory.GetCommand(commandName);
+            return command != null ? command.Execute(args) : "Unknown command";
+        }
     }
 }
